c39cb89fdc952adc5d76d76233ee8fafbf72d58e
li.feng@rograndec.com <li.feng@rograndec.com>
2018-09-18 20:26:37 +0800

    队列的相关操作

--- src/queueDemo/Queue.java
+++ src/queueDemo/Queue.java
@@ -6,11 +6,11 @@ package queueDemo;
  * @CreateDate: 2018/9/18
  * @Version: 3.0
  */
-
+//队列基本上是这几种操作
 public interface Queue<E> {
     //向队列中添加元素，相当于在数组的尾部添加元素
     void enQueue(E e);
-    //向队列中删除元素，相当于在头部删除元素
+    //向队列中删除元素，相当于在数组first头部删除元素
     void deQueue();
     int getSize();
     E getFront();
--- src/stackDemo/Stack.java
+++ src/stackDemo/Stack.java
@@ -10,11 +10,11 @@ package stackDemo;
 public interface Stack<E> {
     //查看栈元素个数
     int getSize();
-    //向栈中放入元素
+    //向栈中放入元素，相当于向数组的末尾添加元素
      void push(E e);
-    //从栈中取出元素
+    //从栈中取出元素，相当于从数组的末尾取元素
     E pop();
-    //查看栈顶元素
+    //查看栈顶元素，相当于查看数组中的末尾元素
     E peek();
     //判断栈中元素是否为空
     Boolean isEmpty();
