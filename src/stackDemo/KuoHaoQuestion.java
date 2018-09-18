f55b90167599e6ed6f29412d58b71b4a14efb4ba
li.feng@rograndec.com <li.feng@rograndec.com>
2018-09-18 18:02:55 +0800

    leetcode括号匹配问题

--- src/stackDemo/KuoHaoQuestion.java
+++ src/stackDemo/KuoHaoQuestion.java
@@ -36,7 +36,7 @@ public class KuoHaoQuestion {
         return stack.isEmpty();
     }
     public static void main(String[] args){
-        System.out.println(new KuoHaoQuestion().isMatch("{}"));
+        System.out.println(new KuoHaoQuestion().isMatch("{[]}"));
     }
 
 }
