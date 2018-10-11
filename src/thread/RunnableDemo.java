1dddbcf862fade9e697f5cdc0bd2d19567388743
li.feng@rograndec.com <li.feng@rograndec.com>
2018-10-11 09:52:05 +0800

    多线程的知识

--- src/thread/RunnableDemo.java
+++ src/thread/RunnableDemo.java
@@ -26,5 +26,17 @@ public class RunnableDemo {
         new Thread(new RunThread("线程2")).start();
         new Thread(new RunThread("线程3")).start();
     }
+    /* Thread与Runnable的关系：这里建议看下Thread的源码：
+     Thread类：class Thread implements Runnable {所以Thread是Runnable的子类
+     构造方法：public Thread(Runnable target) 而target执行了这个run()方法。而这个run()方法被我们覆写了
+      public void run() {
+        if (target != null) {
+            target.run();
+        }
+    }
+     在进行Tread中的start()方法启动多线程时，会调用run()方法。
+     这个过程是：在创建Tread的时候会传入一个Runnable的对象（这里就是：new RunThread("线程1")），
+     这个对象被Thread的构造方法中的Runnable类型的target属性接收，然后target执行run()方法。
+     而这个run()方法，就是上面RunThread类中复写的Runnable的run()方法。*/
 
 }
