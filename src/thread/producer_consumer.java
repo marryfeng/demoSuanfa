0f9974904c27ad0a5c3f93b74dae0d365531d703
li.feng@rograndec.com <li.feng@rograndec.com>
2018-10-11 20:07:10 +0800

    生产者消费者问题

--- src/thread/producer_consumer.java
+++ src/thread/producer_consumer.java
@@ -6,7 +6,46 @@ package thread;
  * @CreateDate: 2018/10/11
  * @Version: 1.0
  */
+//message类：商品库，因为都是对这个message操作，所以同步和等待唤醒都是在这里进行：set与get
+class Message{
+    private String title;
+    private String content;
+    //设置一个信号灯：当flag为true时，生产者可以生产,不允许消费。当flag为false的时候，消费者可以消费
+    private boolean flag=true;
 
+    public synchronized void set(String title, String content) {
+        //生产者不能生产，等待
+        if (this.flag==false){
+            try {
+                super.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.title=title;
+        this.content = content;
+        //已经生产过了
+        this.flag=false;
+        super.notify();
+    }
+
+
+    public synchronized String get() {
+        if (this.flag==true) {
+            try {
+                super.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try{
+            return this.title+this.content;
+        } finally {
+            this.flag=true;
+            super.notify();
+        }
+    }
+}
 //生产者:线程
 class Producer implements Runnable{
        private Message msg;
@@ -38,46 +77,7 @@ public Consumer(Message msg){
         }
     }
 }
-//message类：商品库
-class Message{
-    private String title;
-    private String content;
-    //设置一个信号灯：当flag为true时，生产者可以生产,不允许消费。当flag为false的时候，消费者可以消费
-    private boolean flag=true;
 
-    public synchronized void set(String title, String content) {
-            //生产者不能生产，等待
-        if (this.flag==false){
-        try {
-            super.wait();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-        this.title=title;
-        this.content = content;
-        //已经生产过了
-        this.flag=false;
-        super.notify();
-    }
-
-
-    public synchronized String get() {
-        if (this.flag==true) {
-            try {
-                super.wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-            try{
-                return this.title+this.content;
-            } finally {
-                this.flag=true;
-                super.notify();
-            }
-        }
-}
 
 
 //生产者消费者问题:三个角色：生产者、商品库、消费者；生产者向商品库中生产商品，消费者从商品库中消费商品
