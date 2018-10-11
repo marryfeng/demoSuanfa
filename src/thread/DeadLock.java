ea0548f745e30b468d1a30909d0f74fd7c621733
li.feng@rograndec.com <li.feng@rograndec.com>
2018-10-11 17:00:43 +0800

    死锁

---
+++ src/thread/DeadLock.java
@@ -0,0 +1,13 @@
+package thread;
+
+/**
+ * @Author: marryFeng
+ * @Email: 1473357859@qq.com
+ * @CreateDate: 2018/10/11
+ * @Version: 1.0
+ */
+//死锁：互相等待，互不相让。
+    //若干给线程访问同一资源时要进行同步处理，而过多的同步会造成死锁。死锁属于程序调试的一种。
+    //可以使用工具：jconsole，visualVM来检测死锁
+public class DeadLock {
+}
